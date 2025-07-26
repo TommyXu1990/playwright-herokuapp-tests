52f5af0afae88f40c71836f218e4f81bd5cf111b
TX <Tommy.xu90@gmail.com>
2025-07-26 19:30:17 +0700

    Initial commit: Playwright login automation test

---
+++ tests/login.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.pause();
+  await page.goto('https://the-internet.herokuapp.com/login');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
+  await page.getByRole('button', { name: ' Login' }).click();
+  await page.locator('html').click();
+  await expect(page.locator('h4')).toContainText('Welcome to the Secure Area. When you are done click logout below.');
+  await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible();
+});
