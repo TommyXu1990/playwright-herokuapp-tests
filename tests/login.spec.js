a691755d90fa934f22f93699b366013e72664921
TX <Tommy.xu90@gmail.com>
2025-07-28 20:03:23 +0700

    Refactor

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,16 +1,26 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Login and logout flow', async ({ page }) => {
+  // Pause the test and open Playwright Inspector for step-by-step debugging.
+  // Comment out or remove this line if you don’t want the inspector to open.
   await page.pause();
+
+  //Go to login page
   await page.goto('https://the-internet.herokuapp.com/login');
-  await page.getByRole('textbox', { name: 'Username' }).click();
+
+  //Fills in username and password 
   await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
-  await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
+
+  //Click the login button 
   await page.getByRole('button', { name: ' Login' }).click();
-  await page.locator('html').click();
+
+  //Assert the success message 
   await expect(page.locator('h4')).toContainText('Welcome to the Secure Area. When you are done click logout below.');
-  await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible();
+
+  //Click logout
   await page.getByRole('link', { name: 'Logout' }).click();
-  await page.getByText('You logged out of the secure').click();
+
+  //Assert the logout message 
+  await page.getByText('You logged out of the secure area!').click();
 });
